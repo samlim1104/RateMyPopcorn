9379a46c02a33104a5a5c8a0bc9690391e003ba5
= <=>
2023-04-25 09:25:09 -0700

    retrofit

--- app/src/main/java/com/example/ratemypopcorn/RetrofitHelper.kt
+++ app/src/main/java/com/example/ratemypopcorn/RetrofitHelper.kt
@@ -1,5 +1,9 @@
 package com.example.ratemypopcorn
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 object RetrofitHelper {
     val baseURL = "https://developer.movieglu.com/api-index/cinemadetails/"
 
