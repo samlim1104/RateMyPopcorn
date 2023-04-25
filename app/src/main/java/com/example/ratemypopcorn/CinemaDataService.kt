d0d4f4c79c48521b69a78f819ebfb5a8ebea06e3
Lucas DuBria <dubrialucas21@gmail.com>
2023-04-25 08:47:54 -0700

    3

--- app/src/main/java/com/example/ratemypopcorn/CinemaDataService.kt
+++ app/src/main/java/com/example/ratemypopcorn/CinemaDataService.kt
@@ -1,4 +1,9 @@
 package com.example.ratemypopcorn
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface CinemaDataService {
+    @GET("geolocation")
+    fun getFishDataBySpecies(): Call<List<CinemaData>>
 }
--- app/src/main/java/com/example/ratemypopcorn/RetrofitHelper.kt
+++ app/src/main/java/com/example/ratemypopcorn/RetrofitHelper.kt
@@ -1,5 +1,8 @@
 package com.example.ratemypopcorn
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitHelper {
     val baseURL = "https://developer.movieglu.com/api-index/cinemadetails/"
 
