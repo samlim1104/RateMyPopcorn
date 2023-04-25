9be81ec3fe6ad6fe4222937ab3d84569e742a850
masonsohn23 <masonsohn23@gmail.com>
2023-04-25 09:03:41 -0700

    initial commit

---
+++ app/src/main/java/com/example/ratemypopcorn/MovieAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.ratemypopcorn
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter(var dataset: List<CinemaData>): RecyclerView.Adapter<MovieAdapter.ViewHolder>()
+{
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val textViewName: TextView
+        val textViewRating: TextView
+        val layout: Constants
+
+        init {
+            textViewName = view.findViewById(R.id.textView_cinema_name)
+            textViewRating = view.findViewById(R.id.textView_cinema_rating)
+            layout = view.findViewById(R.id.layout_cinema)
+        }
+    }
+}
